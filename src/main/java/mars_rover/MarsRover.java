ee165fc4372daa947855426f494e1cc44339e21a
Ignacio Saporiti <isaporiti@Ignacios-MacBook-Pro.local>
2020-08-12 09:48:30 +0200

    Moving forward N times.

--- src/main/java/mars_rover/MarsRover.java
+++ src/main/java/mars_rover/MarsRover.java
@@ -2,8 +2,14 @@ package mars_rover;
 
 public class MarsRover {
 
-	public String execute(String string) {
-		return "0:1:N";
+	private Integer yCoordinate = 0;
+
+	public String execute(String command) {
+		for (String subcommand : command.split("")) {
+			yCoordinate++;
+		}
+
+		return "0:"+ yCoordinate + ":N";
 	}
 
 }
--- src/test/java/mars_rover/MarsRoverShould.java
+++ src/test/java/mars_rover/MarsRoverShould.java
@@ -5,8 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class MarsRoverShould {
-    @Test void move_forward_when_receiving_M_command() {
+
+    @Test void move_forward_as_many_times_as_M_commands() {
         MarsRover marsRover = new MarsRover();
         assertEquals("0:1:N", marsRover.execute("M"));
+        assertEquals("0:3:N", marsRover.execute("MM"));
+        assertEquals("0:9:N", marsRover.execute("MMMMMM"));
     }
 }
